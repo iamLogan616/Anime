6b950640bd43c2947921cc23cdf52270b465f342
iamLogan616 <yedhuku2006@gmail.com>
2026-02-04 14:13:12 +0530

    Update __init__.py
    New

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,13 +1,11 @@
 #(©)Codexbotz
 #@iryme
 
-
-
-
-
 from aiohttp import web
 from .route import routes
 
+# No need to import specific modules if they're loaded elsewhere
+# The Bot.on_message decorators will register automatically
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
