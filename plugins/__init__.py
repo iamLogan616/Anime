a7974ce6a6cdc08b5e8ae7748630d4444c084188
iamLogan616 <yedhuku2006@gmail.com>
2026-02-05 07:22:37 +0530

    Update __init__.py
    Fix

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,15 +1,23 @@
-#(©)Codexbotz
-#@iryme
-
-
-
-
-
+# __init__.py in plugins folder
 from aiohttp import web
 from .route import routes
-
+from . import (
+    start,
+    channel_post,
+    link_generator,
+    # Add this line for flink command
+    flink_command
+)
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
+
+# Export all handlers
+__all__ = [
+    'start',
+    'channel_post', 
+    'link_generator',
+    'flink_command'
+]
