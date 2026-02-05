677f2ab752e6feeb7a91dae0fdb61c043187cb22
iamLogan616 <yedhuku2006@gmail.com>
2026-02-05 08:15:59 +0530

    Update __init__.py
    Old

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,13 +1,15 @@
 #(©)Codexbotz
 #@iryme
 
+
+
+
+
 from aiohttp import web
+from .route import routes
+
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
-    
-    # Import routes inside function to avoid circular imports
-    from .route import routes
     web_app.add_routes(routes)
-    
     return web_app
