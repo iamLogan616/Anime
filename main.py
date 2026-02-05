765bd0a2087b60215915ec8171038452016b54b9
iamLogan616 <yedhuku2006@gmail.com>
2026-02-05 08:24:36 +0530

    Update main.py
    Old

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bot import Bot
-import pyrofork.utils
+import pyrogram.utils
 
-pyrofork.utils.MIN_CHANNEL_ID = -1009147483647
+pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
 
 if __name__ == "__main__":
     Bot().run()
