fa3a1c0a60f26bc4de4cb26a0aacfd366c737cb5
iamLogan616 <yedhuku2006@gmail.com>
2026-02-05 08:20:14 +0530

    Update link_generator.py
    Old

--- plugins/link_generator.py
+++ plugins/link_generator.py
@@ -1,8 +1,9 @@
 #(©)Codexbotz
 
-from pyrofork import Client, filters
-from pyrofork.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
-from pyrofork.types import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from bot import Bot
+from pyrogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove
 from asyncio import TimeoutError
 from helper_func import encode, get_message_id, admin
 from config import PERMANENT_LINKS, BLOGSPOT_URL, BLOGSPOT_PARAM
