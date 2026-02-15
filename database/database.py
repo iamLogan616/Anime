f13d628ef755a98ce075cfdd8033d24227ca6689
iamLogan616 <yedhuku2006@gmail.com>
2026-02-15 09:52:00 +0530

    Update database.py
    Extra

--- database/database.py
+++ database/database.py
@@ -9,6 +9,7 @@ from config import DB_URI, DB_NAME
 from bot import Bot
 import logging
 from datetime import datetime, timedelta
+import base64
 
 dbclient = pymongo.MongoClient(DB_URI)
 database = dbclient[DB_NAME]
@@ -31,7 +32,7 @@ class Rohit:
         self.fsub_data = self.database['fsub']   
         self.rqst_fsub_data = self.database['request_forcesub']
         self.rqst_fsub_Channel_data = self.database['request_forcesub_channel']
-        
+        self.seq_batches = self.database['seq_batches']   # new collection
 
 
     # USER DATA
@@ -193,6 +194,30 @@ class Rohit:
         else:
             #print(f"Channel {channel_id} NOT found in the database.")
             return False
+
+
+    # ---------- NEW: SEQUENCE BATCH METHODS ----------
+    async def create_seq_batch(self, channel_type, message_ids, titles, batch_name=None):
+        """Create a new sequence batch and return its unique ID."""
+        batch_id = base64.urlsafe_b64encode(os.urandom(6)).decode().rstrip('=')
+        data = {
+            '_id': batch_id,
+            'channel_type': channel_type,          # "primary" or "secondary"
+            'message_ids': message_ids,
+            'titles': titles,
+            'batch_name': batch_name,
+            'created_at': datetime.utcnow()
+        }
+        await self.seq_batches.insert_one(data)
+        return batch_id
+
+    async def get_seq_batch(self, batch_id):
+        """Retrieve a sequence batch by its ID."""
+        return await self.seq_batches.find_one({'_id': batch_id})
+
+    async def delete_seq_batch(self, batch_id):
+        """Delete a sequence batch."""
+        await self.seq_batches.delete_one({'_id': batch_id})
 
 
 db = Rohit(DB_URI, DB_NAME)
