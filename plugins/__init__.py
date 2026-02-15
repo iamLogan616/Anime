f271a25c4033e77e6074edd90083da219fb2f3dd
iamLogan616 <yedhuku2006@gmail.com>
2026-02-15 06:22:03 +0530

    Update __init__.py
    Add

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,13 +1,17 @@
-#(©)Codexbotz
-#@iryme
-
-
-
-
+#(©)Codeflix_Bots
+# Support @rohit_1888 on Tg
 
 from aiohttp import web
 from .route import routes
 
+# Import all plugin modules
+from .start import *
+from .channel_post import *
+from .link_generator import *
+from .admin import *
+from .sequence_batch import *  # Add this line
+from .callbacks import *
+from .database import *
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
