63d9550660169432ec982a979d6553db2079b862
iamLogan616 <yedhuku2006@gmail.com>
2026-02-15 09:04:20 +0530

    Add files via upload
    Old

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,17 +1,13 @@
-#(©)Codeflix_Bots
-# Support @rohit_1888 on Tg
+#(©)Codexbotz
+#@iryme
+
+
+
+
 
 from aiohttp import web
 from .route import routes
 
-# Import all plugin modules
-from .start import *
-from .channel_post import *
-from .link_generator import *
-from .admin import *
-from .sequence_batch import *  # Add this line
-from .callbacks import *
-from .database import *
 
 async def web_server():
     web_app = web.Application(client_max_size=30000000)
